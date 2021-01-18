697474c2e934fa0286744cec9680ff4edffcd96b
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-01-18 22:12:12 +0530

    ADD: New plugin - Image shart for pre render image loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,20 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
         path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `articles`,
+        path: `${__dirname}/content/articles`,
       },
     },
     `gatsby-plugin-mdx`,
