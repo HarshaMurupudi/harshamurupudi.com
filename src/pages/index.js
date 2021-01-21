ca7b7ef381c0706960bae9a52cb7a0e5d0e44f4c
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-01-21 13:43:44 +0530

    ADD: Index page mdx articles query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,34 @@
 import React from "react"
+import { graphql } from 'gatsby';
 
-export default function Home() {
+const IndexPage = ({ data }) => {
+  console.log(data);
   return <div>Hello world!</div>
 }
+
+export const query = graphql`
+{
+  allMdx(sort: {fields: frontmatter___date, order: DESC}, limit: 2) {
+    nodes {
+      excerpt
+      frontmatter {
+        title
+        slug
+        readTime
+        category
+        date(formatString: "MMM, Do, YYYY")
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+      id
+    }
+  }
+}
+`
+
+export default IndexPage;
