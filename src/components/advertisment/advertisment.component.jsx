257ae24354d1874f94bcfa4dee5a1af6f2d15c00
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-05-12 18:23:00 +0530

    ADD: Basic styling to advertisment section in home page

--- src/components/advertisment/advertisment.component.jsx
+++ src/components/advertisment/advertisment.component.jsx
@@ -4,13 +4,13 @@ import './advertisment.styles.scss'
 
 const Advertisment = () => {
   return (
-    <div className="advertisment-container">
-      <div>
-        <h5>Get healthy</h5>
-        <h5>Raw Honey</h5>
+    <div className="advertisment">
+      <div className="advertisment__heading">
+        <h3>Get healthy</h3>
+        <h3>Raw Honey</h3>
       </div>
       <p>At Dr. Queen Bee we provide it raw & unprocessed</p>
-      <div>
+      <div className="advertisment__footer">
         <div>
           <span>Img</span>
           <span>Img</span>
