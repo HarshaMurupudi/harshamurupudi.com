6df02f858a561d4b26f1df13b7f2216a60c86d04
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-05-15 08:41:53 +0530

    ADD: Styling to subscribe section of home page

--- src/components/subscribe/subscribe.component.jsx
+++ src/components/subscribe/subscribe.component.jsx
@@ -5,16 +5,28 @@ import './subscribe.styles.scss'
 const Subscribe = () => {
   return (
     <div className="subscribe">
-      <h6>
+      <h3>
         Don't wanna miss ?
-    </h6>
-      <p>Get an email update for every new edition</p>
-      <div>
-        <input type="text" />
-        <input type="text" />
+      </h3>
+      <h4>Get an email update for every new edition</h4>
+      <div className="subscribe__input-fields">
+        <div className="order-1 d-inline col-12 col-md-8 floating-form">
+          <input className="floating-input" type="text" placeholder=" " id="email" name="email" aria-describedby="email" />
+          <span class="highlight"></span>
+          <label htmlFor="email">Name</label>
+        </div>
+        <div className="order-1 d-inline col-12 col-md-8 floating-form">
+          <input className="floating-input" type="text" placeholder=" " id="email" name="email" aria-describedby="email" />
+          <span class="highlight"></span>
+          <label htmlFor="email">Email</label>
+        </div>
+
       </div>
-      <button>Subscribe</button>
+      <div className="subscribe__submit-btn">
+        <button>Subscribe</button>
+      </div>
     </div>
+
   )
 }
 
