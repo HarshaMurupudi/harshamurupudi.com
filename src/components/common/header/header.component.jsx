10bfe4372364753e49a63293243e3f8a320d76f1
harsha Murupudi <harsha@dmaassociatescorp.com>
2021-05-30 15:30:24 +0530

    ADD: Placehoder icon for nav hamburger icon

--- src/components/common/header/header.component.jsx
+++ src/components/common/header/header.component.jsx
@@ -5,13 +5,21 @@ import Logo from '../../header/logo-section.component'
 import FindMe from '../../header/find-me.component'
 import NavBar from './navbar.component'
 
+import { FaAlignRight } from "react-icons/fa"
+
+
 const Header = () => {
   return (
     <div>
       <div className="home-page-grid-container">
         <Author />
         <Logo />
-        <FindMe />
+        <div>
+          <FindMe />
+          <button type="button" className="toggle-btn">
+            <FaAlignRight />
+          </button>
+        </div>
       </div>
       <hr />
       <NavBar />
