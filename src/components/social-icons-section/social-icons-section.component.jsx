8ba8a73270df6510a075a9137ec2923105374741
harsha Murupudi <harsha@dmaassociatescorp.com>
2021-06-14 21:10:51 +0530

    ADD: Basic component boiler plate code for social links section component

---
+++ src/components/social-icons-section/social-icons-section.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import socialLinksData from '../../constants/socialLinks'
+
+import './social-icons-section.styles.scss'
+
+const SocialIconsSection = ({ }) => {
+  return (<div>Social Icons</div>)
+};
+
+export default SocialIconsSection;
